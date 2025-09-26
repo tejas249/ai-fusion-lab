7eb7642bd69d611d951754b1489d1d4f01c1140c
Tejas Kamble <tejaskambletk132@gmail.com>
2025-09-26 12:21:33 +0530

    Setup and themes

---
+++ app/provider.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+
+function Provider({
+    children, ...props
+}) {
+  return (
+    <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+    {...props}>
+    <div>
+       {children}
+    </div>
+    </NextThemesProvider>
+    
+  )
+}
+
+export default Provider
