76ecb3618ff3ead17afc5945f0adba3b96906172
Tejas Kamble <tejaskambletk132@gmail.com>
2025-09-26 13:57:48 +0530

    Sidebar added

---
+++ app/_components/AppSidebar.jsx
@@ -0,0 +1,70 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+import { Ghost } from "lucide-react";
+import Image from "next/image";
+import { Sun , Moon} from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function AppSidebar() {
+    const {theme, setTheme} = useTheme();
+  return (
+    <Sidebar>
+        <SidebarHeader>
+       
+       <div className="p-3">
+        <div className=" flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+                <Image src={'/logo.svg'} alt="Logo" width={60} height={60}
+              className="w-[40px] h-[40px] rounded-lg object-cover"
+            />
+            <h2 className="font-bold text-xl"> AI FUSION </h2>
+            </div>
+         
+  {theme === "light" ? (
+    <Button variant="ghost" onClick={() => setTheme("dark")}>
+      <Sun />
+    </Button>
+  ) : (
+    <Button variant="ghost" onClick={() => setTheme("light")}>
+      <Moon />
+    </Button>
+  )}
+</div>
+
+    <div>
+
+         <Button className='mt-2 w-full ' size='lg'> + New Chat </Button>
+       </div>   
+
+ </div>
+</SidebarHeader>
+   
+
+
+      <SidebarContent>
+        
+       <SidebarGroup>
+        <div className={'p-3'}>
+         <h2 className="font-bold text-lg">Chat</h2>
+         <p className="text-sm text-gray-400 ">Sign in to start chatting with multiple AI Models</p>
+         </div>
+       </SidebarGroup>
+
+      </SidebarContent>
+      
+
+      <SidebarFooter>
+        <div className="p-3 mb-10">
+            <Button className="w-full">Sign In / Sign Up</Button>
+        </div>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from './_components/AppSidebar'
 
 function Provider({
     children, ...props
 }) {
   return (
-    <NextThemesProvider
+    <NextThemesProvider  {...props}
             attribute="class"
             defaultTheme="dark"
             enableSystem
-            disableTransitionOnChange
-    {...props}>
-    <div>
-       {children}
-    </div>
+            disableTransitionOnChange>
+        <SidebarProvider>
+            <AppSidebar/>
+            <SidebarTrigger/>
+              <div>
+                   {children}
+              </div>
+        </SidebarProvider>
+   
     </NextThemesProvider>
     
   )
